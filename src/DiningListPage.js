fc50ecae861a18d14b9cd70b47078d10cb4031a6
Siddhant Dalvi <93437464+siddhant-dalvi@users.noreply.github.com>
2024-02-24 12:31:56 +0530

    Update DiningListPage.js

--- src/DiningListPage.js
+++ src/DiningListPage.js
@@ -100,7 +100,7 @@ const DiningListPage = () => {
         </select>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
         {sortedProducts.map(product => (
           <div
             key={product.id}
